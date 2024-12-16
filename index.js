6c6eb17000253746a8f989432b63930f3ffd5f59
aiman <aimanrifq@gmail.com>
2024-12-16 14:26:50 +0800

    initial commit

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  res.json({
+    ipaddress: "::ffff:159.20.14.100",
+    language: "en-US,en;q=0.5",
+    software: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"
+  });
 });
 
 // listen for requests :)
